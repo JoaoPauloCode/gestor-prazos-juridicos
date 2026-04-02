ca0b9cbcac6700fb32ea8706294532cf59e1570c
João Paulo <jopabarcelos@gmail.com>
2026-04-02 11:25:18 -0300

    Versão inicial - backend + frontend funcionando

--- static/script.js
+++ static/script.js
@@ -1,3 +1,42 @@
+const feriados = [
+  "2026-01-01",
+  "2026-02-16",
+  "2026-02-17",
+  "2026-02-18",
+  "2026-04-03",
+  "2026-04-05",
+  "2026-04-21",
+  "2026-05-01",
+  "2026-06-04",
+  "2026-09-07",
+  "2026-10-12",
+  "2026-11-02",
+  "2026-11-15",
+  "2026-11-20",
+  "2026-12-25"
+];
+
+function calcularPrazo(dataInicial, dias) {
+  let data = new Date(dataInicial);
+  let adicionados = 0;
+
+  while (adicionados < dias) {
+    data.setDate(data.getDate() + 1);
+
+    const diaSemana = data.getDay();
+    const dataISO = data.toISOString().split("T")[0];
+
+    const ehFimDeSemana = (diaSemana === 0 || diaSemana === 6);
+    const ehFeriado = feriados.includes(dataISO);
+
+    if (!ehFimDeSemana && !ehFeriado) {
+      adicionados++;
+    }
+  }
+
+  return data.toISOString().split("T")[0];
+}
+
 function formatarData(dataIso) {
   const [ano, mes, dia] = dataIso.split('-');
   return `${dia}/${mes}/${ano}`;
@@ -9,16 +48,23 @@ async function cadastrar() {
   const data_inicial = document.getElementById("data").value;
   const dias = document.getElementById("dias").value;
 
+ 
+  const prazoFinal = calcularPrazo(data_inicial, parseInt(dias));
+
   await fetch("/processos", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ numero, nome, data_inicial, dias })
+    body: JSON.stringify({
+      numero,
+      nome,
+      prazo: prazoFinal
+    })
   });
 
   listar();
-  alert("Processo cadastrado com sucesso!");
+  mostrarMensagem("Processo cadastrado com sucesso!", "sucesso");
 }
 
 async function listar() {
@@ -27,13 +73,15 @@ async function listar() {
 
   const processos = resultado.dados;
 
-  const lista = document.getElementById("lista");
-  lista.innerHTML = "";
+  const lista = document.getElementById("lista"); 
+  lista.innerHTML = ""; 
 
   processos.forEach(p => {
+    const dataFormatada = formatarData(p.prazo);
+
     lista.innerHTML += `
       <div class="processo">
-        <span>${p.numero} - ${p.nome} - ${p.prazo}</span>
+        <span>${p.numero} - ${p.nome} - ${dataFormatada}</span>
         <button class="btn-delete" onclick="deletar('${p.numero}')">
           Excluir
         </button>
@@ -50,9 +98,12 @@ async function buscar() {
 
   if (result.sucesso) {
     const p = result.dados;
-    alert(`Processo encontrado:\nNúmero: ${p.numero}\nNome: ${p.nome}\nPrazo: ${formatarData(p.prazo)}`);
+    mostrarMensagem(
+      `Processo: ${p.numero} - ${p.nome} - ${formatarData(p.prazo)}`,
+      "sucesso"
+    );
   } else {
-    alert(result.erro);
+    mostrarMensagem(result.erro, "erro");
   }
 }
 
@@ -75,5 +126,7 @@ async function deletar(numero) {
   });
 
   listar();
+  mostrarMensagem("Processo excluído com sucesso!", "sucesso");
 }
+
 window.onload = listar;
